3d0accc041718d9acca28248d5f80855695dfc8a
oluobamzy <oluobamzy@gmail.com>
2023-07-31 12:33:56 -0400

    add contributions and stories to tables

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -8,3 +8,16 @@ INSERT INTO
   users (username, PASSWORD)
 VALUES
   ('Kira', 'password');
+INSERT INTO
+  users (username, PASSWORD)
+VALUES
+  ('Shajee', 'password');
+INSERT INTO
+  users (username, PASSWORD)
+VALUES
+  ('Natasha', 'password');
+  INSERT INTO
+  users (username, PASSWORD)
+VALUES
+  ('Olu', 'password');
+
---
+++ db/seeds/03_contributions.sql
@@ -0,0 +1,16 @@
+INSERT INTO contributions (story_id, user_id, contribution_content, up_vote, created_at)
+VALUES (1, 3, 'What a thrilling start!', 10, '2023-07-31 12:00:00');
+INSERT INTO contributions (story_id, user_id, contribution_content, up_vote, created_at)
+VALUES (1, 2, 'The hero faced a daunting challenge.', 5, '2023-07-30 15:30:00');
+INSERT INTO contributions (story_id, user_id, contribution_content, up_vote, created_at)
+VALUES (2, 1, 'The mystery deepens...', 8, '2023-07-29 08:45:00');
+INSERT INTO contributions (story_id, user_id, contribution_content, up_vote, created_at)
+VALUES (3, 4, 'The lost artifact is said to grant great powers.', 12, '2023-07-28 11:15:00');
+INSERT INTO contributions (story_id, user_id, contribution_content, up_vote, created_at)
+VALUES (4, 2, 'The spaceship encounters a strange alien species.', 6, '2023-07-27 09:30:00');
+INSERT INTO contributions (story_id, user_id, contribution_content, up_vote, created_at)
+VALUES (4, 3, 'The alien species turns out to be friendly!', 20, '2023-07-26 17:00:00');
+INSERT INTO contributions (story_id, user_id, contribution_content, up_vote, created_at)
+VALUES (3, 1, 'The hero finds a hidden passage.', 3, '2023-07-25 14:20:00');
+INSERT INTO contributions (story_id, user_id, contribution_content, up_vote, created_at)
+VALUES (2, 4, 'The old mansion is filled with ghostly apparitions.', 7, '2023-07-24 22:10:00');
