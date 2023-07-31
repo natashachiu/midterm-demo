5b2167edf5ae74dbd51bffa8e71652d2eabbeb9a
Shajee-j <Shajeejm@gmail.com>
2023-07-31 11:35:27 -0400

    stories db table

---
+++ db/schema/02_stories.sql
@@ -0,0 +1,11 @@
+CREATE TABLE stories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(100) NOT NULL,
+  description VARCHAR(500),
+  content TEXT NOT NULL,
+  completed BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP DEFAULT NOW(),
+  is_fav BOOLEAN DEFAULT false,
+  up_vote SMALLINT
+);
